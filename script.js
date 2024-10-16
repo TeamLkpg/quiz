d530e043fd3d133639dca5ddc8087d23e5e12e69
Johanna <152695438+Johanna1066@users.noreply.github.com>
2024-10-16 14:21:19 +0200

    Quiz script sceleton
    Co-authored-by: Oskar Jonsson <ojonss@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const quizdata = [
   {
     question: "What is the smallest planet in our solar system",
     options: ["jupiter", "mercury", "earth", "mars"],
-    hideOrder: [2, 0, 3],
     answer: "mercury",
   },
   {
@@ -13,130 +12,73 @@ const quizdata = [
       "Charles Babbage",
       "Mohammad Abdollahi",
     ],
-    hideOrder: [3, 0, 2],
     answer: "Alan Turing",
   },
   {
     question: "What is the largest mammal in the world?",
     options: ["Blue Whale", "Elephant", "Giraffe", "Hippopotamus"],
-    hideOrder: [2, 1, 3],
     answer: "Blue Whale",
   },
   {
     question: "What is the capital of Spain?",
     options: ["Barcelona", "Madrid", "Seville", "Valencia"],
-    hideOrder: [2, 0, 3],
     answer: "Madrid",
   },
   {
     question: "What is the largest planet in our solar system?",
     options: ["jupiter", "mercury", "earth", "mars"],
-    hideOrder: [2, 1, 3],
     answer: "jupiter",
   },
 ];
 
-/*
-<div id="q1" class="question">
-                <p><span class="qno">1</span></p>
-                <input type="radio" name="rdbQ1" value="-1" onclick="UpdateScore(this)" /><br />
-                <input type="radio" name="rdbQ1" value="-1" onclick="UpdateScore(this)" /><br />
-                <input type="radio" name="rdbQ1" value="1" onclick="UpdateScore(this)"  /><br />
-                <input type="radio" name="rdbQ1" value="-1" onclick="UpdateScore(this)" /><br />
-            </div>
-            De här är det vi vill fast med script
-*/
-
 const questionElement = document.getElementById("question");
 const optionsElement = document.getElementById("options");
+var questionIndex = 0;
 
-/* Vidare till nästa fråga
-const nextQuestionButton = document.getElementById(nextBtn);
-*/
-
-
-let score = 0;
-let currentQuestion = 0;
-let currentQuestionTries = 0;
-
+for (let i of optionsElement.children) {
+  i.addEventListener("click", selectOption);
+}
 
 function showQuestion() {
-    showTries();
-    const question = quizdata[currentQuestion];
-    questionElement.innerText = question.question;
-   
-    optionsElement.innerHTML = "";
-    question.options.forEach((option) => {
-    const button = document.createElement("button");
-    button.innerText = option;
-    optionsElement.appendChild(button);
-    button.addEventListener("click", selectAnswer);
-  });
+  var currentQuestion = quizdata[questionIndex].question;
+  questionElement.innerHTML = currentQuestion;
 }
 
-function showTries(){
-    const tries = document.getElementById("currentTries");
-    tries.innerText = currentQuestionTries + 1;
+function showOptions() {
+  var currentOptions = quizdata[questionIndex].options;
+  for (i = 0; i < currentOptions.length; i++) {
+    optionsElement.children[i].innerHTML = currentOptions[i];
+  }
 }
 
-function selectAnswer(input) {
-    const selectedAnswer = input.target;
-    const answer = quizdata[currentQuestion].answer;
-    currentQuestionTries++;
-
-    if (selectedAnswer.innerText === answer) {
-        score += 5;
-        currentQuestion++;
-        currentQuestionTries = 0;
-        if (currentQuestion > quizdata.length-1) {
-            alert("Quiz is over1, your score is " + score);
-        }
-        currentQuestionTries = 0;
-        showTries();
-        showQuestion();
+function selectOption() {
+  if (this.innerHTML == quizdata[questionIndex].answer) {
+    questionIndex++;
+    if (questionIndex >= quizdata.length) {
+      endOfQuiz();
+      return;
+    } else {
+      resetOptions();
+      showQuestion();
+      showOptions();
     }
-    else if (currentQuestion < quizdata.length) {
-
-        if (currentQuestionTries < quizdata[currentQuestion].options.length) {
-            hideOption(currentQuestionTries);
-            currentQuestionTries = 0;
-            showQuestion();
-        }
-        else {
-
-             alert("All tries are used");
-             /*fail?!*/
-        }
-    } 
-    else {
-        alert("Quiz is over2");
-    }
+  } else {
+    this.style.backgroundColor = "red";
+  }
 }
 
-
-/*Försök till att ändra utskrift*/
-function hideOption(input) {
-  alert("fel");
-  const hideElement = quizdata[currentQuestion].hideOrder[input-1];
-  const hide2 = quizdata.options[hideElement];
-  const optionsElement = document.getElementById(hide2.innerHTML);
-  optionsElement.style.color = "red"; 
-  currentQuestionTries++;
+function endOfQuiz() {
+  alert("Tack för att du deltog!");
+  for (let i of optionsElement.children) {
+    i.removeEventListener("click", selectOption);
+  }
 }
 
-
-
-/*
-  const hideButton = document.getElementById("hide-button");
-  const alternativeToHide = document.getElementById(options); // Lägg in element att byta ut
-
-  hideButton.addEventListener("click", function () {alternativeToHide.classList.toggle("hidden")});
-*/
+function resetOptions() {
+  for (let i of optionsElement.children) {
+    i.style.backgroundColor = "white";
+  }
+}
 
 showQuestion();
-
-function initialAnswers()
-            {
-                for(var j=1;j<document.getElementsByClassName("question").length;++j)
-                    answers[j] = 0
-            }
+showOptions();
